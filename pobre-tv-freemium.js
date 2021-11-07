14fec4b4ef7294ec26feff564f0485803bb0a5b8
Paulo Gil <paulogil93@gmail.com>
2021-11-07 22:52:31 +0000

    Create pobre-tv-freemium.js

---
+++ pobre-tv-freemium.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Pobre.TV Freemium
+// @namespace    mailto:paulogil93@gmail.com 
+// @version      1.0
+// @description  Make Pobre.TV Free Again!
+// @author       Paulo Gil
+// @match        https://pobre.tv/*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        window.NFU9EMROIFMERJOFM = false;
+    }, false);
+})();
